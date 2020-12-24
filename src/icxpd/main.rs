fd07135d1bfa82a2a8691dded2fd7287ddbf3b2f
nshou <nshou@coronocoya.net>
2020-12-23 18:18:51 -0800

    Ensure ~/.icxp exists

--- src/icxpd/main.rs
+++ src/icxpd/main.rs
@@ -1,12 +1,15 @@
 pub mod commons;
-
 mod core;
+
 use crate::core::listener::unix_socket_listener::UnixSocketListener;
+use std::fs;
 
 fn main() {
     println!("Hello, ICXPd!");
 
     let c = commons::Commons::init().unwrap();
+
+    fs::create_dir_all(c.get_work_dir().unwrap()).unwrap();
 
     let l = UnixSocketListener::new(&c).unwrap();
     l.listen().unwrap();
