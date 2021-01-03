bad4373ac5e9557288b5d545b9ef623f19f15f87
nshou <nshou@coronocoya.net>
2021-01-02 17:36:19 -0800

    Add serde_json to dependencies and a factory function to Command impl

--- src/icxpd/core/command/mod.rs
+++ src/icxpd/core/command/mod.rs
@@ -1,4 +1,19 @@
-#[derive(Debug)]
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
 pub enum Command {
-    NOP(String),
+    NOP(Nop),
+}
+
+#[derive(Deserialize, Debug)]
+struct Nop {
+    extras: Option<String>,
+}
+
+impl Command {
+    pub fn from_json(j: &String) -> Result<Command, String> {
+        //TODO: unify errors and propagate serde_json::Result
+        let cmd: Result<Command, _> = serde_json::from_str(&j);
+        cmd.or(Err(String::from("json parser error")))
+    }
 }
