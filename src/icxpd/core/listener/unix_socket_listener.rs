c834369613602e9c596014551f6f5da8b3dd3c84
nshou <nshou@coronocoya.net>
2021-01-24 00:08:21 -0800

    Use mpsc to control UnixSocketListener's listen thread instead of using shared memory

--- src/icxpd/core/listener/unix_socket_listener.rs
+++ src/icxpd/core/listener/unix_socket_listener.rs
@@ -2,24 +2,26 @@ use crate::commons::Commons;
 use std::path::{Path, PathBuf};
 use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{UnixListener, UnixStream};
-use tokio::sync::mpsc::Sender;
+use tokio::sync::mpsc::{self, Receiver, Sender};
 use tokio::time::{self, Duration};
 
 const SOCK_NAME: &str = "icxpd.sock";
+const CTL_BRIDGE_QLEN: usize = 8;
 
-#[derive(Debug)]
 pub struct UnixSocketListener {
     sock_path: PathBuf,
-    command_sender: Sender<String>,
     state: UnixSocketListenerState,
+    ctl: Sender<UnixSocketListenerCtl>,
 }
 
-#[derive(Debug)]
 enum UnixSocketListenerState {
-    Initialized,
     Running,
     Closing,
     Closed,
+}
+
+enum UnixSocketListenerCtl {
+    Close,
 }
 
 #[derive(Debug)]
@@ -29,147 +31,56 @@ pub enum UnixSocketListenerError {
 }
 
 impl UnixSocketListener {
-    pub fn new(c: &Commons) -> Result<UnixSocketListener, UnixSocketListenerError> {
+    pub fn listen(c: &Commons) -> Result<UnixSocketListener, UnixSocketListenerError> {
         let work_dir = c.get_work_dir().ok_or_else(|| {
-            UnixSocketListenerError::Generic(String::from("Invalid home directory path"))
+            UnixSocketListenerError::Generic(String::from(
+                "Failed to look up the path to place Unix socket file",
+            ))
         })?;
         let mut sock_path = PathBuf::from(work_dir);
         sock_path.push(SOCK_NAME);
+        let listener = UnixListener::bind(sock_path.as_path())?;
         let command_sender = c.get_command_sender();
+        let (ctl_sender, ctl_receiver) = mpsc::channel(CTL_BRIDGE_QLEN);
+
+        tokio::spawn(async move {
+            loop {
+                match listener.accept().await {
+                    Ok((stream, _addr)) => {
+                        let command_sender_clone = command_sender.clone();
+                        tokio::spawn(async move {
+                            let reader = BufReader::new(stream);
+                            let mut lines = reader.lines();
+                            // Multiple input lines work as a batch
+                            //TODO: use logger instead of unwrap
+                            while let Some(line) = lines.next_line().await.unwrap() {
+                                //TODO: use logger instead of unwrap
+                                command_sender_clone.send(line).await.unwrap();
+                            }
+                        });
+                    }
+                    // Accepting a connection can lead to various errors
+                    // and not all of them are necessarily fatal
+                    Err(e) => {
+                        //TODO: use logger
+                        println!("error while accepting: {:?}", e);
+                    }
+                }
+
+                //TODO: ctl match
+            }
+        });
+
         Ok(UnixSocketListener {
             sock_path,
-            command_sender,
-            state: UnixSocketListenerState::Initialized,
+            state: UnixSocketListenerState::Running,
+            ctl: ctl_sender,
         })
-    }
-
-    pub fn get_sock_path(&self) -> Option<&str> {
-        self.sock_path.to_str()
-    }
-
-    pub async fn listen(&mut self) -> Result<(), UnixSocketListenerError> {
-        let listener = UnixListener::bind(&self.sock_path.as_path())?;
-        self.state = UnixSocketListenerState::Running;
-        while let UnixSocketListenerState::Running = self.state {
-            match listener.accept().await {
-                Ok((stream, _addr)) => {
-                    tokio::spawn(UnixSocketListener::wt_handle(
-                        stream,
-                        self.command_sender.clone(),
-                    ));
-                }
-                // Accepting a connection can lead to various errors
-                // and not all of them are necessarily fatal
-                //TODO: use logger
-                Err(e) => println!("error while accepting: {:?}", e),
-            }
-        }
-        //TODO: teardown sock pfile
-        self.state = UnixSocketListenerState::Closed;
-        Ok(())
-    }
-
-    async fn wt_handle(stream: UnixStream, command_sender: Sender<String>) {
-        let reader = BufReader::new(stream);
-        let mut lines = reader.lines();
-        // Multiple input lines work as a batch
-        while let Some(line) = lines.next_line().await.unwrap() {
-            command_sender.send(line).await.unwrap();
-        }
-    }
-
-    pub async fn shutdown(&mut self) -> Result<(), UnixSocketListenerError> {
-        //TODO: early return based on self.state
-
-        let sock_path = String::from(self.get_sock_path().ok_or_else(|| {
-            UnixSocketListenerError::Generic(String::from("Invalid socket file path"))
-        })?);
-        let mut stream: Result<UnixStream, io::Error> =
-            Err(io::Error::new(io::ErrorKind::Other, "na"));
-        // Socket file that l.listen() creates might not be ready
-        for _i in 0_i32..10 {
-            time::sleep(Duration::from_millis(10)).await;
-            stream = UnixStream::connect(Path::new(&sock_path)).await;
-            if let Ok(_) = stream {
-                break;
-            }
-        }
-        let mut stream = stream?;
-
-        stream.writable().await?;
-        self.state = UnixSocketListenerState::Closing;
-        // Ignore errors here. Other normal traffic could come in first
-        stream.try_write("{\"NOP\"}".as_bytes()).ok();
-        stream.shutdown().await.ok();
-
-        Ok(())
     }
 }
 
 impl From<io::Error> for UnixSocketListenerError {
     fn from(err: io::Error) -> UnixSocketListenerError {
         UnixSocketListenerError::Io(err)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::fs;
-    use uuid::Uuid;
-
-    fn reserve_test_dir_name() -> String {
-        format!(".icxp_test_{}", Uuid::new_v4().to_hyphenated().to_string())
-    }
-
-    #[tokio::test]
-    async fn one_message_delivery() {
-        let msg = "one_message_delivery";
-        let test_dir_name = reserve_test_dir_name();
-        let mut c = Commons::init(Some(&test_dir_name)).unwrap();
-        fs::create_dir_all(c.get_work_dir().unwrap()).unwrap();
-
-        let mut l = UnixSocketListener::new(&c).unwrap();
-        let sock_path = String::from(l.get_sock_path().unwrap());
-        let listener = tokio::spawn(async move {
-            l.listen().await.unwrap();
-        });
-
-        let mut stream: Result<UnixStream, io::Error> =
-            Err(io::Error::new(io::ErrorKind::Other, "na"));
-        // Socket file that l.listen() creates might not be ready
-        for _i in 0_i32..10 {
-            time::sleep(Duration::from_millis(10)).await;
-            stream = UnixStream::connect(Path::new(&sock_path)).await;
-            if let Ok(_) = stream {
-                break;
-            }
-        }
-        // Let it panic if failed for 10 times
-        let mut stream = stream.unwrap();
-
-        stream.writable().await.unwrap();
-        assert_eq!(msg.len(), stream.try_write(msg.as_bytes()).unwrap());
-        stream.shutdown().await.unwrap();
-
-        assert_eq!(
-            Some(String::from(msg)),
-            c.get_command_receiver().recv().await
-        );
-
-        //TODO: what if either sender/receiver is closed?
-        // cases: sender dropped / receiver dropped / receiver closed
-        // e.g. if sender is dropped:
-        //   assert_eq!(None, c.get_command_receiver().recv().await);
-        // tests should be added after gentle shutdown implemented
-
-        //TODO: panic when thread panics
-        //        let (ul_r,) = tokio::join!(ul);
-        //        if let Err(e) = ul_r {
-        //            println!("error occurred while joining {:?}: {:?}", l, e);
-        //        }
-
-        //TODO: teardown
-        //close listener and delete test dir
     }
 }
--- src/icxpd/main.rs
+++ src/icxpd/main.rs
@@ -11,12 +11,14 @@ async fn main() {
 
     fs::create_dir_all(c.get_work_dir().unwrap()).unwrap();
 
-    let mut l = UnixSocketListener::new(&c).unwrap();
-    let ul = l.listen();
+    let listener = UnixSocketListener::listen(&c).unwrap();
+
+    //TODO: heartbeat loop on current thread
+    // immediately exits for now
 
     //TODO: try_join? select?
-    let (ul_r,) = tokio::join!(ul);
-    if let Err(e) = ul_r {
-        println!("error occurred while joining {:?}: {:?}", l, e);
-    }
+    //    let (ul_r,) = tokio::join!(ul);
+    //    if let Err(e) = ul_r {
+    //        println!("error occurred while joining {:?}: {:?}", l, e);
+    //    }
 }
